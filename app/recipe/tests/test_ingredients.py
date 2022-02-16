f9b81b7265046017e805cbb8b8df1d255a01f68d
M. Brock Podgurski <mbpod10@gmail.com>
2022-02-16 15:04:32 -0500

    add ingredient object and endpoints

---
+++ app/recipe/tests/test_ingredients.py
@@ -0,0 +1,81 @@
+from urllib import response
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Ingredient
+
+from recipe.serializers import IngredientSerializer
+
+INGREDIENTS_URL = reverse('recipe:ingredient-list')
+
+
+class PublicIngredientsApiTest(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        """Test that an unauthorized user cannot get list"""
+        response = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PriveIngredientsApiTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            "test@gmail.com",
+            "password123"
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_tags(self):
+        """Test retrieving tags"""
+        Ingredient.objects.create(user=self.user, name='Kale')
+        Ingredient.objects.create(user=self.user, name='Orange')
+
+        res = self.client.get(INGREDIENTS_URL)
+
+        tags = Ingredient.objects.all().order_by('-name')
+        serializer = IngredientSerializer(tags, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_tags_limited_to_user(self):
+        """Test that tags returned are for authenticated user"""
+        user2 = get_user_model().objects.create_user(
+            'other@londonappdev.com',
+            'testpass'
+        )
+        Ingredient.objects.create(user=user2, name='Salt')
+        tag = Ingredient.objects.create(user=self.user, name='Pepper')
+
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], tag.name)
+
+    # def test_create_tag_successful(self):
+    #     """Test creating a new tag"""
+    #     payload = {'name': 'Simple'}
+    #     self.client.post(INGREDIENTS_URL, payload)
+
+    #     exists = Ingredient.objects.filter(
+    #         user=self.user,
+    #         name=payload['name']
+    #     ).exists()
+    #     self.assertTrue(exists)
+
+    # def test_create_tag_invalid(self):
+    #     """Test creating a new tag with invalid payload"""
+    #     payload = {'name': ''}
+    #     res = self.client.post(INGREDIENTS_URL, payload)
+
+    #     self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,13 +2,16 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
-    """Manage tags in the database"""
+class TagViewSet(viewsets.GenericViewSet,
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
+    """Manage TAGS in the database"""
+
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     queryset = Tag.objects.all()
@@ -17,3 +20,26 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new tag"""
+        serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(viewsets.GenericViewSet,
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
+    """Manage INGREDIENTS in the database"""
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.TagSerializer
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new ingredient"""
+        serializer.save(user=self.request.user)
